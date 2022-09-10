8b3bf08379234432c8da636b6bb63e69ca86c6c6
Doguhan Yangoz <doguhan.yangoez@elproAG.onmicrosoft.com>
2022-09-10 11:03:54 +0200

    CardsProvider has wrapped.

--- src/components/pages/App.tsx
+++ src/components/pages/App.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Suspense } from 'react';
 import styled, { css, createGlobalStyle } from 'styled-components/macro'
 import { Input } from '../../toDelete/Input'
 import { Routes, Route } from 'react-router-dom'
@@ -6,6 +6,7 @@ import { Layout } from '../widgets/Layout'
 import { Home } from '../widgets/Home'
 import { Cards } from './Cards';
 import { CardEdit } from './CardEdit';
+import { CardsProvider } from 'infrastructure/CardsContext';
 
 
 
@@ -13,21 +14,24 @@ function App() {
 
   return (
     <>
-     <Routes>
-      <Route path="/" element={<Layout />}>
-        <Route index element={<Home />} />
-        <Route path="cards" element={<Cards />} />
-        <Route path="cards/:id" element={<CardEdit />} />
-          <Route
-            path="*"
-            element={
-              <main style={{ padding: "1rem" }}>
-                <p>There's nothing here!</p>
-              </main>
-            }
-          />
-      </Route>
-    </Routes>
+      <CardsProvider>
+        <Routes>
+          <Route path="/" element={<Layout />}>
+            <Route index element={<Home />} />
+            <Route path="cards" element={<Cards />} />
+            <Route path="cards/:id" element={<CardEdit />} />
+            <Route
+              path="*"
+              element={
+                <main style={{ padding: "1rem" }}>
+                  <p>There's nothing here!</p>
+                </main>
+              }
+            />
+          </Route>
+        </Routes>
+      </CardsProvider>
+
     </>
   )
 }
