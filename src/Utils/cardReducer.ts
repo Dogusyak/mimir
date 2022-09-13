86ab568e930b218bf705dde05c1d34a581cbb07b
Doguhan Yangoz <doguhan.yangoez@elproAG.onmicrosoft.com>
2022-09-13 08:12:15 +0200

    CardReducer has been modified.

--- src/Utils/cardReducer.ts
+++ src/Utils/cardReducer.ts
@@ -8,25 +8,25 @@ import CardService from 'services/CardService'
 export function cardReducer(state: State, action: Action): State {
   switch (action.type) {  
     case 'add-card':
-      let existingCard = state.cards.find(
-        card => card.id === action.card.id
-      )
-          if (existingCard) {
-              const updateCard = {
-                  ...existingCard,
-                  front: action.card.front,
-                  back: action.card.back
-              }
-        return {
-          ...state,
-          cards: state.cards.map(card => {
-            if (card.id === updateCard?.id) {
-              return updateCard
-            }
-            return card
-          })
-        }
-      }
+      // let existingCard = state.cards.find(
+      //   card => card.id === action.card.id
+      // )
+      //     if (existingCard) {
+      //         const updateCard = {
+      //             ...existingCard,
+      //             front: action.card.front,
+      //             back: action.card.back
+      //         }
+      //   return {
+      //     ...state,
+      //     cards: state.cards.map(card => {
+      //       if (card.id === updateCard?.id) {
+      //         return updateCard
+      //       }
+      //       return card
+      //     })
+      //   }
+      // }
       const add = async () => {
         const response = await CardService.addCard(createCard(action.card));
         if (response === 'success') {
@@ -36,7 +36,7 @@ export function cardReducer(state: State, action: Action): State {
       add()
       return {
         ...state,
-        cards: [...state.cards, createCard(action.card)]
+        // cards: [...state.cards, createCard(action.card)]
       }
     case 'remove-card':
       return {
